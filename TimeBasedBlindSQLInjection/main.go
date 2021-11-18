ee4cda0f2156837d92c9b37f7c973beeed8b8f7a
gurbanli <gurbanliaydin@gmail.com>
2021-11-18 17:39:07 +0400

    add time based sql injection app

---
+++ TimeBasedBlindSQLInjection/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"SQLInjection/controller"
+	"SQLInjection/repository"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	repo := repository.Repository{}
+	repo.InitializeDatabase()
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("session_id", store))
+
+	loginController := controller.LoginController{}
+	productController := controller.ProductController{}
+
+	router.POST("/login", loginController.LoginByUsername)
+	router.GET("/products/:id", productController.GetProduct)
+
+	router.Run(":4444")
+}
